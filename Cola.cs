719b13f641c5bf6448fde7a4f190002ee2e6add6
Joel M <59495685+naGiX1212@users.noreply.github.com>
2024-12-13 14:08:23 -0300

    cola

--- Cola.cs
+++ Cola.cs
@@ -1,12 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AlgoritmosYEstrucutrasDeDatos;
 
 namespace AlgoritmosYEstructurasDeDatos
 {
+    
     public class Cola
     {
+        private Nodo? nodoPri;
+        private Nodo? nodoUlt;
+        public void CrearCola()
+        {
+            nodoPri = null;
+            nodoUlt = null;
+        }
+        public void PonerEnCola(int n)
+        {
+            Nodo nuevoNodo = new Nodo { n = n, Sig = null };
+
+            if (nodoUlt == null) // Si la cola está vacía
+            {
+                nodoPri = nodoUlt = nuevoNodo; 
+            }
+            else
+            {
+                nodoUlt.Sig = nuevoNodo; 
+                nodoUlt = nuevoNodo;
+            }
+        }
+        public int? VerTope()
+        {
+            return nodoPri != null? nodoPri.n : null; 
+        }
+
+        public int? SacarDeCola()
+        {
+            if (nodoPri == null)
+                return null;
+            if (nodoPri == nodoUlt)
+            {
+                nodoPri = nodoUlt = null;
+            }
+            else
+            {
+                nodoPri = nodoPri.Sig;
+            }
+            
+            return nodoPri?.n;
+
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,23 +1,40 @@
-﻿namespace AlgoritmosYEstrucutrasDeDatos
+﻿using AlgoritmosYEstructurasDeDatos;
+
+namespace AlgoritmosYEstrucutrasDeDatos
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Pila c = new Pila();
+            ///////PILA
+            Pila p = new Pila();
             Console.WriteLine("Agrego Elementos a la Pila");
             for (int i = 0; i < 10; i++)
             {
-                c.ponerEnCola(i);
-                Console.WriteLine(c.verTope());
+                p.ponerEnCola(i);
+                Console.WriteLine(p.verTope());
             }
             Console.WriteLine("\nElimino elementos de la Pila");
             for (int i = 0; i < 10; i++)
             {
 
-                Console.WriteLine(c.verTope());
-                c.sacarDeCola();
+                Console.WriteLine(p.verTope());
+                p.sacarDeCola();
             }
+            ///////////COLA
+            Cola c = new Cola();
+            Console.WriteLine("Pongo elementos en la COLA : 0,1,2,3,4,5,6,7,8,9");
+            for (int i = 0; i < 10; i++)
+            {
+                c.PonerEnCola(i);
+            }
+            Console.WriteLine("Elimino elementos de la COLA");
+            for (int i = 0; i < 10; i++)
+            {
+                c.SacarDeCola();
+                Console.WriteLine(c.VerTope());
+            }
+
             
         }
     }
